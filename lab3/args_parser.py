a4faf0017749715227db774fe6ea2c6bc462a3f1
APMorozov <morozov-sasha200@yandex.ru>
2024-10-23 20:39:16 +0400

    Update args_parser.py

--- lab3/args_parser.py
+++ lab3/args_parser.py
@@ -2,6 +2,10 @@ import argparse
 
 
 def arg_parser() -> tuple:
+    '''
+    Parse args from console input
+    :return: tuple of PATH to image and PATH to folder where will be saved processed image 
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('pathtoimage', type=str, help='PATH to image,which will be processed')
     parser.add_argument('-ptf', '--pathtofolder', type=str, help='PATH to folder where will be save image')
