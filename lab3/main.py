5ff0d875bc0635216ad7fac08599961e0d5cb5be
APMorozov <morozov-sasha200@yandex.ru>
2024-10-25 21:21:08 +0400

    Fix main.py

--- lab3/main.py
+++ lab3/main.py
@@ -1,8 +1,7 @@
 import cv2
-import os
 
 from args_parser import arg_parser
-from make_histogram import make_histogram
+from make_histogram import make_histogram, calc_histogram
 from process_img import process2binary, showimg, getresolution
 from read_file import read_file
 
@@ -17,16 +16,17 @@ def main():
     except ProcessLookupError:
         print("Ошибка чтения файла")
     try:
-        make_histogram(img)
+        make_histogram(calc_histogram(img))
     except Exception:
         print("Ошибка построения гистограммы")
     try:
         binary_img = process2binary(img)
         showimg(binary_img, "Binary img")
+        make_histogram(calc_histogram(binary_img))
     except Exception:
         print("Ошибка обработки изображения в бинарное")
     try:
-        cv2.imwrite(os.path.join(path_to_folder, "result.jpg"), binary_img)
+        cv2.imwrite(path_to_folder, binary_img)
     except Exception:
         print("Ошибка записи бинарного изображения")
 
