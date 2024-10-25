1a9c17be0c50161a742ceb5a40e27077b8a2fcf0
APMorozov <morozov-sasha200@yandex.ru>
2024-10-25 21:22:49 +0400

    Split Func make_histogram.py

--- lab3/make_histogram.py
+++ lab3/make_histogram.py
@@ -3,25 +3,39 @@ import matplotlib.pyplot as plt
 from numpy import ndarray
 
 
-def make_histogram(img: ndarray) -> None:
+def calc_histogram(img: ndarray) -> list:
     '''
-    Make histogram of image
-    :param img: image which will be process in histogram
-    :return: None
+    Calculation of histogram
+    :param img: img which be conversed into histogram
+    :return: list of ndarray. It is a histograms sorted by channels
     '''
     channels = cv2.split(img)
+    if len(channels) == 3:
+        histb = cv2.calcHist([img], [0], None, [256], [0, 256])
+        histr = cv2.calcHist([img], [1], None, [256], [0, 256])
+        histg = cv2.calcHist([img], [2], None, [256], [0, 256])
+        hists = [histb, histr, histg]
+        return hists
+    if len(channels) == 1:
+        histbin = cv2.calcHist([img], [0], None, [256], [0, 256])
+        hists = [histbin]
+        return hists
+
+
+def make_histogram(hists: list) -> None:
+    '''
+    Make histogram of image
+    :param hists: list of ndarray,which store histogram sorted by channels(b,r,g) or binary
+    :return: None
+    '''
     plt.xlabel("Тон пикселя")
     plt.ylabel("Частота")
     plt.title("Гистограмма изображения по каналам")
-    if len(channels) == 3:
-        hist1 = cv2.calcHist([img], [0], None, [256], [0, 256])
-        plt.plot(hist1,label='Blue channel', color='b')
-        hist2 = cv2.calcHist([img], [1], None, [256], [0, 256])
-        plt.plot(hist2, label='Red channel', color='r')
-        hist3 = cv2.calcHist([img], [2], None, [256], [0, 256])
-        plt.plot(hist3, label='Green channel', color='g')
-    if len(channels) == 1:
-        hist1 = cv2.calcHist([img], [0], None, [256], [0, 256])
-        plt.plot(hist1, label='Binary channel')
+    if len(hists) == 3:
+        plt.plot(hists[0], label='Blue channel', color='b')
+        plt.plot(hists[1], label='Red channel', color='r')
+        plt.plot(hists[2], label='Green channel', color='g')
+    if len(hists) == 1:
+        plt.plot(hists[0], label='Binary channel')
     plt.legend()
     plt.show()
