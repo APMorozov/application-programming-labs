cad59bfcbb50a9c044283d0250510003af659c52
APMorozov <morozov-sasha200@yandex.ru>
2024-11-05 10:49:49 +0400

    Fix main.py

--- lab4/main.py
+++ lab4/main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 from args_parser import args_parser
-from dataframe import read_file, calcWHC, sortbymaxWH, add_resolution, sort_by_resolution
-from histogram import calc_hist
+from dataframe import read_file, calcWHC, sortbymaxWH, add_resolution, sort_by_resolution, get_static
+from histogramm import calc_hist
 
 
 
@@ -10,42 +10,30 @@ from histogram import calc_hist
 def main():
     pd.options.display.max_rows = None
     pd.options.display.max_columns = None
-
-    csvFile = args_parser()
-
     try:
+        csvFile = args_parser()
+
         data = read_file(csvFile)
         print('Start df\n', data)
-    except Exception as ex:
-        print("Read file error:", ex)
 
-    try:
         dataWHC = calcWHC(data)
         print('df with width height and channels\n', dataWHC)
-    except Exception as ex:
-        print("Calc WHC error:", ex)
 
-    try:
-        sortedWHData = sortbymaxWH(dataWHC, 800, 900)
+
+        sortedWHData = sortbymaxWH(dataWHC, 1000, 1000)
         print('Sorted df by max width and height\n', sortedWHData)
-    except Exception as ex:
-        print("Sort by WH error:", ex)
 
-    try:
         dataWithRes = add_resolution(dataWHC)
         print('df with resolution\n', dataWHC)
-    except Exception as ex:
-        print("Add resolution Error:", ex)
 
-    try:
         sortedResData = sort_by_resolution(dataWithRes)
         print('sorted df by resolution\n', dataWHC)
-    except Exception as ex:
-        print("sort by resolution error:", ex)
-    try:
         calc_hist(sortedResData)
+        print("Statistic Data:")
+        get_static(dataWithRes)
     except Exception as ex:
-        print("Draw hist error:", ex)
+        print('Error:', ex)
+
 
 
 if __name__ == "__main__":
